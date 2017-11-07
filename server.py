f15ed94de5f24d937b55301e918c7140364e7d03
azoulayDan <dan.azoulay@imerir.com>
2017-11-07 20:13:51 +0100

    Second commit pour déploiement avec ajout de la route d'initialisation à la base de données

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 from flask import Flask, make_response, redirect
 from flask_cors import CORS 
 import json
+from db import Db
 
 app = Flask(__name__)
 app.debug = True
@@ -12,11 +13,26 @@ def response(data, status=200):
 	resp = make_response(json.dumps(data), status)
 	resp.mimetype = 'application/json'
 	return resp
+
+def test():
+	db = Db()
+	count = db.select("SELECT COUNT(*) FROM Compte")
+	print(count)
+	db.close()
+	return count
 ####
+
+@app.route('/debug/db/reset', methods=['GET'])
+def init_db():
+	db = Db()
+	db.executeFile('database.sql')
+	db.close()
+	return 'Database OK'
 
 @app.route('/')
 def index():
-	data = {"hello":"hello world man"}
+	essaie = test()
+	data = {"hello":"hello world man", "test":essaie}
 	return response(data, 200)
 
 if __name__ == '__main__':

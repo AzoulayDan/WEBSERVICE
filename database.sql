a5efa00495f221f0583419116cb88777b6d47911
azoulayDan <dan.azoulay@imerir.com>
2017-11-08 09:52:38 +0100

    Quinzième commit: correction des erreurs produits par les commentaires mal realisé dans le fichier database.sql

--- database.sql
+++ database.sql
@@ -2,10 +2,7 @@ DROP TABLE IF EXISTS Compte CASCADE;
 DROP TABLE IF EXISTS Mission CASCADE;
 DROP TABLE IF EXISTS Photo CASCADE;
 
-#------------------------------------------------------------
-# Table: Compte
-#------------------------------------------------------------
-
+-- Table: Compte
 CREATE TABLE Compte(
         id_compte       int (11) Auto_increment  NOT NULL ,
         login_compte    Varchar (35) ,
@@ -16,20 +13,14 @@ CREATE TABLE Compte(
         UNIQUE (login_compte )
 )ENGINE=InnoDB;
 
-#------------------------------------------------------------
-# Table: Mission
-#------------------------------------------------------------
-
+-- Table: Mission
 CREATE TABLE Mission(
         id_mission   int (11) Auto_increment  NOT NULL ,
         name_mission Varchar (35) ,
         PRIMARY KEY (id_mission )
 )ENGINE=InnoDB;
 
-#------------------------------------------------------------
-# Table: Photo
-#------------------------------------------------------------
-
+-- Table: Photo
 CREATE TABLE Photo(
         id_photo   int (11) Auto_increment  NOT NULL ,
         name_photo Varchar (35) ,
@@ -38,36 +29,30 @@ CREATE TABLE Photo(
         UNIQUE (name_photo )
 )ENGINE=InnoDB;
 
-#------------------------------------------------------------
-# Table: participer
-#------------------------------------------------------------
-
+-- Table: participer
 CREATE TABLE participer(
         id_compte  Int NOT NULL ,
         id_mission Int NOT NULL ,
         PRIMARY KEY (id_compte ,id_mission )
 )ENGINE=InnoDB;
 
-#------------------------------------------------------------
-# Table: avoir
-#------------------------------------------------------------
 
+-- Table: avoir
 CREATE TABLE avoir(
         id_photo   Int NOT NULL ,
         id_mission Int NOT NULL ,
         PRIMARY KEY (id_photo ,id_mission )
 )ENGINE=InnoDB;
 
-#------------------------------------------------------------
-# Table: trouver
-#------------------------------------------------------------
 
+-- Table: trouver
 CREATE TABLE trouver(
         id_compte Int NOT NULL ,
         id_photo  Int NOT NULL ,
         PRIMARY KEY (id_compte ,id_photo )
 )ENGINE=InnoDB;
 
+-- Alter table
 ALTER TABLE participer ADD CONSTRAINT FK_participer_id_compte FOREIGN KEY (id_compte) REFERENCES Compte(id_compte);
 ALTER TABLE participer ADD CONSTRAINT FK_participer_id_mission FOREIGN KEY (id_mission) REFERENCES Mission(id_mission);
 ALTER TABLE avoir ADD CONSTRAINT FK_avoir_id_photo FOREIGN KEY (id_photo) REFERENCES Photo(id_photo);
@@ -75,20 +60,8 @@ ALTER TABLE avoir ADD CONSTRAINT FK_avoir_id_mission FOREIGN KEY (id_mission) RE
 ALTER TABLE trouver ADD CONSTRAINT FK_trouver_id_compte FOREIGN KEY (id_compte) REFERENCES Compte(id_compte);
 ALTER TABLE trouver ADD CONSTRAINT FK_trouver_id_photo FOREIGN KEY (id_photo) REFERENCES Photo(id_photo);
 
+-- Insert to test
 INSERT INTO Compte(login_compte, password_compte, role_compte, points_compte) VALUES 
 ('toto', 'password', 'admin', 0),
 ('babar', 'password2', 'team', 0),
 ('elephant', 'password3', 'coucou', 0); 
-
-#CREATE TABLE Compte(
-#	id_compte		SERIAL PRIMARY KEY,
-#	login_compte		Varchar(25),
-#	password_compte		Varchar(25),
-#	role_compte		Varchar(25)
-#);
-
-
-#INSERT INTO Compte(login_compte, password_compte, role_compte) VALUES
-#	('toto','password','admin'),
-#	('babar', 'password2', 'team'),
-#	('elephant', 'password3', 'coucou');
